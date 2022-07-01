c937a4f9c50865d583afb8139591f3477c87b477
HAIDALA <91802818+HAIDALA@users.noreply.github.com>
2022-07-01 19:57:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
-from typing import Mapping
+
 from neuralintents import GenericAssistant
 import speech_recognition
 import pyttsx3 as tts
 import sys
-import json
 
 recognizer = speech_recognition.Recognizer()
-
+fr_voice_id ="HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_FR-FR_HORTENSE_11.0"
 speaker = tts.init()
-speaker.setProperty('rate',150)   ## how fast you want your assistant to talk
-
-todo_list = ['Go Shopping', 'Clean Room', 'Record Video']
-
+speaker.setProperty('voice', fr_voice_id)
+speaker.setProperty('rate',180)   ## how fast you want your assistant to talk
 
 
 
-def add_Sopping_list():
 
+Shopping_list = ['parfum', 'Cafe', "pomme" ]
+
+def create_shopping_list():
     global recognizer
-    speaker.say("What item do you want to add to your shopping list?")
+
+    speaker.say("Que souhaitez-vous ajouter à votre liste d'achat?")
     speaker.runAndWait()
+
     done=False
+
     while not done:
         try:
 
@@ -28,43 +30,93 @@ def add_Sopping_list():
                 recognizer.adjust_for_ambient_noise(mic, duration=0.2)
                 audio = recognizer.listen(mic)
 
-                item = recognizer.recognize_google(audio)
+                note = recognizer.recognize_google(audio, language="fr-FR")
+                note = note.lower()
+
+                speaker.say("Choisissez un nom de liste d'achat")
+                speaker.runAndWait()
+
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = recognizer.listen(mic)
+
+                filename = recognizer.recognize_google(audio, language="fr-FR")
+                filename = filename.lower()
+            
+            with open(filename, 'w') as f:
+                f.write(note)
+                done = True
+                speaker.say("J'ai créé avec succès la liste d'achat {filename}")
+                speaker.runAndWait()
+
+        except speech_recognition.UnKnownValueError():
+            recognizer = speech_recognition.Recognizer()
+            speaker.say("je n'ai pas compris! Veuillez réessayer!")
+            speaker.runAndWait()
+
+
+def add_item_shopping_list():
+
+    global recognizer
+
+    speaker.say("Quel article voulez-vous ajouter?")
+    speaker.runAndWait()
+                
+    done=False
+
+    while not done:
+        try:
+
+            with speech_recognition.Microphone() as mic:
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = recognizer.listen(mic)
+
+                item = recognizer.recognize_google(audio, language="fr-FR")
                 item = item.lower()
 
-                todo_list.append(item)
+                Shopping_list.append(item)
                 done = True
 
-                speaker.say("I added {item} to my shopping list!")
+                speaker.say(f"J'ai ajouter {item} dans votre list d'achat!")
                 speaker.runAndWait()
 
         except speech_recognition.UnKnownValueError:
             recognizer = speech_recognition.Recognizer()
-            speaker.say("I did not undrstand you!. Please try again!")
+            speaker.say("je n'ai pas compris! Veuillez réessayer!")
             speaker.runAndWait()
 
-def show_Sopping_list():
+def show_Shopping_list():
 
-    speaker.say("The items on your shopping list are the following")
-    for item in todo_list:
+    speaker.say("les articles dans votre liste d'achat sont")
+    for item in Shopping_list:
         speaker.say(item)
     speaker.runAndWait()
 
 def hello():
-    speaker.say("Hello. What can I do for you?")
+    speaker.say("Bonjour. Que puis-je faire pour vous?",)
     speaker.runAndWait()
-
+   
+    
+  
 def quit():
-    speaker.say("Bye")
+    speaker.say("Au revoir")
     speaker.runAndWait()
     sys.exit(0)
 
 
+mappings ={
+    "greeting":hello,
+    "create_shopping_list": create_shopping_list,
+    "add_item_shopping_list":add_item_shopping_list,
+    "show_Shopping_list":show_Shopping_list,
+    "exit":quit
+}
 
 
-                
 
 assistant = GenericAssistant('intents.json', intent_methods=mappings)
 assistant.train_model()
+
+
 
 while True:
 
@@ -75,10 +127,25 @@ while True:
             recognizer.adjust_for_ambient_noise(mic, duration=0.2)
             audio = recognizer.listen(mic)
 
-            message = recognizer.recognize_google(audio)
+            message = recognizer.recognize_google(audio, language="fr-FR")
             message = message.lower()
         
         assistant.request(message)
+        
     
     except speech_recognition.UnKnownValueError:
         recognizer = speech_recognition.Recognizer() 
+        speaker.say("je n'ai pas compris! Veuillez réessayer!")
+        speaker.runAndWait()
+
+
+
+    
+
+        
+
+
+
+
+
+
